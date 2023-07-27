e12fb31461763d3c38261f24cf09a9e519cfd787
Shreyaan Pradhan <shreyaan.pradhan2020@vitstudent.ac.in>
2023-07-27 17:10:11 +0530

    added use effect

--- pages/products/view.js
+++ pages/products/view.js
@@ -1,16 +1,31 @@
 import { useRouter } from 'next/router';
-import Layout from "@/component/Layout";
+import Layout from '@/component/Layout';
 import { useState, useEffect } from 'react';
-export default function viewProduct(){
-    const router = useRouter();
-    const {data} = router.query;
-    const myObject = JSON.parse(data);
-    return(
-        <Layout>
-            <h1>Hello this is
-                {myObject}
-            </h1>
-        </Layout>
 
-    )
+export default function ViewProduct() {
+  const router = useRouter();
+  const { data } = router.query;
+  const [myObject, setMyObject] = useState(null);
+
+  // Parse data once it is available
+  useEffect(() => {
+    if (data) {
+      try {
+        setMyObject(JSON.parse(data));
+      } catch (error) {
+        console.error('Error parsing JSON data:', error);
+      }
+    }
+  }, [data]);
+
+  return (
+    <Layout>
+      <h1>Hello this is</h1>
+      {myObject ? (
+        <pre>{JSON.stringify(myObject, null, 2)}</pre>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </Layout>
+  );
 }
