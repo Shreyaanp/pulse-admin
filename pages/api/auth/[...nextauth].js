49084be3f42ab771c64f08bbdc3d5d369f24b605
Shreyaan Pradhan <shreyaan.pradhan2020@vitstudent.ac.in>
2023-07-29 14:56:22 +0530

    build process

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -11,53 +11,6 @@ export default NextAuth({
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
-  callbacks: {
-    async signIn(user, account, profile) {
-      // Handle sign-in logic and errors if needed
-      try {
-        // Your sign-in logic here
-        return true; // Return true if sign-in is successful
-      } catch (error) {
-        // Handle sign-in errors
-        console.error('Sign-in error:', error.message);
-        return false; // Return false if sign-in fails
-      }
-    },
-
-    async session(session, user) {
-      // Handle session logic and errors if needed
-      try {
-        // Your session logic here
-        return session; // Return the updated session object
-      } catch (error) {
-        // Handle session errors
-        console.error('Session error:', error.message);
-        return session; // Return the unmodified session object
-      }
-    },
-
-    async jwt(token, user, account, profile, isNewUser) {
-      // Handle JSON Web Token (jwt) logic and errors if needed
-      try {
-        // Your jwt logic here
-        return token; // Return the updated token
-      } catch (error) {
-        // Handle jwt errors
-        console.error('JWT error:', error.message);
-        return token; // Return the unmodified token
-      }
-    },
-
-    async redirect(url, baseUrl) {
-      // Handle redirect logic and errors if needed
-      try {
-        // Your redirect logic here
-        return url; // Return the redirect URL
-      } catch (error) {
-        // Handle redirect errors
-        console.error('Redirect error:', error.message);
-        return baseUrl; // Return the original baseUrl
-      }
-    },
-  },
+  secret: process.env.SECRET,
+  
 });
